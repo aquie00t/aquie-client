158fe6e7ffa66ef239edc2de58f91c04e5f3b4d6
aquie00t <aquie00t@icloud.com>
2025-05-28 04:32:43 +0300

    NotFound page title change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import WorkExperience from "../components/WorkExperience";
 import Contact from "../components/Contact";
 import Footer from "../components/Footer";
 
-export default function App() {
+export default function Home() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Navbar />
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,9 @@
+import { useEffect } from "react";
+
 export default function NotFound() {
+  useEffect(() => {
+    document.title = "Aquie - NotFound";
+  }, []);
   return (
     <main className="min-h-screen flex flex-col items-center justify-center text-center px-4">
       <h1 className="text-6xl font-bold mb-4 text-white">404</h1>
