9d6a4b590b1c24813e375c109d5e342e428cbe85
aquie00t <aquie00t@icloud.com>
2025-06-25 01:23:38 +0300

    Not Found page 404 text color change.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   }, []);
   return (
     <main className="min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-6xl font-bold mb-4 text-white">404</h1>
+      <h1 className="text-6xl font-bold mb-4 text-black">404</h1>
       <p className="text-xl text-gray-400 mb-6">
         Oops! The page you’re looking for doesn’t exist.
       </p>
