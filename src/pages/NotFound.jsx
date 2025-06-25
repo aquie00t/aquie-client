a19efbd85e4cf5da41b9fe87e71a4d32acb45320
aquie00t <aquie00t@icloud.com>
2025-06-25 03:32:17 +0300

    NotFound Page updated.

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,14 +5,14 @@ export default function NotFound() {
     document.title = "Aquie - NotFound";
   }, []);
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center text-center px-4">
-      <h1 className="text-6xl font-bold mb-4 text-black">404</h1>
-      <p className="text-xl text-gray-400 mb-6">
+    <main className="min-h-screen flex flex-col items-center justify-center text-center px-4 bg-black">
+      <h1 className="text-6xl font-bold mb-4 text-white">404</h1>
+      <p className="text-xl text-gray-200 mb-6">
         Oops! The page you’re looking for doesn’t exist.
       </p>
       <a
         href="/"
-        className="px-6 py-2 rounded-xl bg-white text-black font-medium hover:bg-gray-200 transition"
+        className="text-white inline-block border border-white px-6 py-2 rounded hover:bg-white hover:text-black transition"
       >
         Go Home
       </a>
